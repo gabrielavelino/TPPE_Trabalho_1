dd9569569235a7505fdd2f66a98f97f26bb3828f
gabrielavelino <blackzinh8@gmail.com>
2022-12-21 11:32:39 -0300

    Adicionando inputs e getters/setters
    Co-authored-by: guilhermemoraisr <guilhermemoraisr@gmail.com>  Co-authored-by: gabrielbpn <gabrielbpn0802@gmail.com>

--- Rendimentos.py
+++ Rendimentos.py
@@ -1,10 +1,20 @@
 
 class Rendimento:
     
-    def __init__(self,descricao,valor):
+    def __init__(self,descricao =  '',valor = 0):
         self.descricao = descricao
         self.valor = valor
         pass
+
+    def getValor(self):
+        return self.valor
+    def setValor(self,valor):
+        self.valor = valor
+    def getDescricao(self):
+        return self.descricao
+    def setDescricao(self,descricao):
+        self.descricao = descricao
+    
 
     def CalculaRendimento(self):
         return self.valor == 1
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from Rendimentos import Rendimento
 
+rendimento = Rendimento()
+
 def main():
-    print("Hello World!")
+    print("============= Calculadora IRPF =============")
+    valor = float(input("Digite o valor do seu rendimento: "))
+    rendimento.setValor(valor)
+    descriçao = str(input("Digite a descrição do seu rendimento: "))
+    rendimento.setDescricao(descriçao)
+    print("O valor do seu rendimento é: ", rendimento.getValor())
+    print("A descrição do seu rendimento é: ", rendimento.getDescricao())
 
 
 if __name__ == "__main__":
